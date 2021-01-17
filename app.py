f77ef935a195118b6ac705e482e36635ddd114e3
Herman Wandabwa <wandabwa2004@gmail.com>
2021-01-18 12:37:47 +1300

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def predict_quality(model, df):
     
     return predictions_data['Label'][0]
     
-model = load_model('Randomforestmodel')
+model = load_model('RF_Model')
 
 
 st.title('Kiwirail Trains OnTime  Predictions')
