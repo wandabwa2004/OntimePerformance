14e3964465a278715a731221281cddb4d1aaefe8
Herman Wandabwa <wandabwa2004@gmail.com>
2021-01-18 12:38:03 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon 11th Jan 2021
-@author: Herman Wandabwa - herman.wandabwa@kiwirail.co.nz
+
 """
 
 from pycaret.classification import load_model, predict_model
